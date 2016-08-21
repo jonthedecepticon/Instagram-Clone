ecb5334e3868ce0701f8bbfc9b2cbfc859b47237
Jon Lambson <jon@crowdengine.com>
2016-08-20 21:59:52 -0600

    added devise

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,49 @@
+class PicturesController < ApplicationController
+  before_action :find_picture, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @picture = current_user.pictures.build
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+
+    if @picture.save
+      redirect_to @picture, notice: "Successfully posted!"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to root_path
+  end
+
+  def update
+    if @picture.update(picture_params)
+      redirect_to @picture, notice: "Post has been updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:title, :description)
+    end
+
+    def find_picture
+      @picture = Picture.find(params[:id])
+    end
+end
